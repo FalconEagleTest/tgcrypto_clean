a363b9dec032372953ec4377ddf0539d39b7541d
FalconEagleTest <34710362+FalconEagleTest@users.noreply.github.com>
2025-02-12 22:12:35 +0200

    Update tgcrypto.c

--- tgcrypto.c
+++ tgcrypto.c
@@ -1,69 +1,52 @@
 // tgcrypto.c
+#include <stdint.h>
+#include <stdlib.h>
 #include "aes256.h"
 #include "ige256.h"
 #include "ctr256.h"
 #include "cbc256.h"
 
-uint8_t* ige256_encrypt(const uint8_t* data, uint32_t length, const uint8_t* key, const uint8_t* iv) {
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT __attribute__((visibility("default")))
+#endif
+
+EXPORT uint8_t* ige256_encrypt(const uint8_t* data, uint32_t length, const uint8_t* key, const uint8_t* iv) {
     if (length == 0 || length % 16 != 0) {
         return NULL;
     }
-    
-    if (!key || !iv) {
-        return NULL;
-    }
-    
     return ige256(data, length, key, iv, 1);
 }
 
-uint8_t* ige256_decrypt(const uint8_t* data, uint32_t length, const uint8_t* key, const uint8_t* iv) {
+EXPORT uint8_t* ige256_decrypt(const uint8_t* data, uint32_t length, const uint8_t* key, const uint8_t* iv) {
     if (length == 0 || length % 16 != 0) {
         return NULL;
     }
-    
-    if (!key || !iv) {
-        return NULL;
-    }
-    
     return ige256(data, length, key, iv, 0);
 }
 
-uint8_t* ctr256_encrypt(const uint8_t* data, uint32_t length, const uint8_t* key, const uint8_t* iv, const uint8_t* state) {
+EXPORT uint8_t* ctr256_encrypt(const uint8_t* data, uint32_t length, const uint8_t* key, uint8_t* iv, uint8_t* state) {
     if (length == 0) {
         return NULL;
     }
-    
-    if (!key || !iv || !state || *state > 15) {
-        return NULL;
-    }
-    
-    return ctr256(data, length, key, (uint8_t*)iv, (uint8_t*)state);
+    return ctr256(data, length, key, iv, state);
 }
 
-uint8_t* ctr256_decrypt(const uint8_t* data, uint32_t length, const uint8_t* key, const uint8_t* iv, const uint8_t* state) {
+EXPORT uint8_t* ctr256_decrypt(const uint8_t* data, uint32_t length, const uint8_t* key, uint8_t* iv, uint8_t* state) {
     return ctr256_encrypt(data, length, key, iv, state);
 }
 
-uint8_t* cbc256_encrypt(const uint8_t* data, uint32_t length, const uint8_t* key, const uint8_t* iv) {
+EXPORT uint8_t* cbc256_encrypt(const uint8_t* data, uint32_t length, const uint8_t* key, uint8_t* iv) {
     if (length == 0 || length % 16 != 0) {
         return NULL;
     }
-    
-    if (!key || !iv) {
-        return NULL;
-    }
-    
-    return cbc256(data, length, key, (uint8_t*)iv, 1);
+    return cbc256(data, length, key, iv, 1);
 }
 
-uint8_t* cbc256_decrypt(const uint8_t* data, uint32_t length, const uint8_t* key, const uint8_t* iv) {
+EXPORT uint8_t* cbc256_decrypt(const uint8_t* data, uint32_t length, const uint8_t* key, uint8_t* iv) {
     if (length == 0 || length % 16 != 0) {
         return NULL;
     }
-    
-    if (!key || !iv) {
-        return NULL;
-    }
-    
-    return cbc256(data, length, key, (uint8_t*)iv, 0);
+    return cbc256(data, length, key, iv, 0);
 }
